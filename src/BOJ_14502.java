b2f1a73d8c6c8f49cd6d266b188a7218a6a59819
inaeLee <inae9709@hanmail.net>
2020-04-06 23:22:51 +0900

    [BOJ]14502_연구소_20/04/06

---
+++ src/BOJ_14502.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BOJ_14502 {
+    static int M, N;
+    static int[][] map;
+    static int[][] mapClone;
+    static ArrayList<Point> virus = new ArrayList<>();
+    static int maxNum = 0;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken()); //세로
+        M = Integer.parseInt(st.nextToken()); //가로
+
+        map = new int[N][M];
+        mapClone = new int[N][M];
+
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j =0; j<M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if( map[i][j] == 2 ) {
+                    virus.add(new Point(i, j));
+                }
+            }
+        }
+
+        setWall(0, 0);
+        System.out.println(maxNum);
+    }
+
+    public static void setWall(int start, int depth) {
+        if (depth == 3) {
+            copyMap();
+
+            for(Point p : virus) {
+                dfs(p.x, p.y);
+            }
+
+            maxNum = Math.max(maxNum, getArea());
+            return;
+        }
+
+        for(int i= start; i < N * M; i++) {
+            int x = i / M;
+            int y = i % M;
+
+            if(map[x][y] == 0) {
+                map[x][y] =1;
+                setWall(i + 1, depth + 1);
+                map[x][y] =0;
+            }
+        }
+    }
+
+    public static void copyMap() {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                mapClone[i][j] = map[i][j];
+            }
+        }
+    }
+
+    public static int getArea() {
+        int count =0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (mapClone[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void dfs(int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                if (mapClone[nx][ny] == 0) {
+                    mapClone[nx][ny] = 2;
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
+
