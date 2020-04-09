bfe37fcca2720b64b9cbf0d0325446e85ea46b1d
inaeLee <inae9709@hanmail.net>
2020-04-09 17:34:16 +0900

    [BOJ]16236_아기상어_20/04/09

--- src/BOJ_16236.java
+++ src/BOJ_16236.java
@@ -6,12 +6,12 @@ import java.util.*;
 public class BOJ_16236 {
     static int shark = 2;
     static int eatFish = 0; //먹은 물고기 수
-    static int totalFish = 0; //map에 있는 물고기 수
     static int count = 0; //소요 시간
-    static ArrayList<Point> fishList = new ArrayList<>();
-    static int N;
+    static int[][] fishDistance; //거리와 방문 여부 체크
     static int[][] map;
-    static Point start;
+    static int N;
+    static Point start; //상어 위치
+    static int minDist, minX, minY;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
 
@@ -21,92 +21,115 @@ public class BOJ_16236 {
         N = Integer.parseInt(br.readLine());
 
         map = new int[N][N];
+        fishDistance = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(1 <= map[i][j] && map[i][j] <= 6) {
-                    fishList.add(new Point(i, j));
-                    totalFish++;
-                } else if(map[i][j] == 9) {
+
+                if(map[i][j] == 9) {
                     start = new Point(i, j);
+                    map[i][j] = 0;
                 }
             }
         }
-        fishSort();
 
-//        while (totalFish != 0) {
-//            bfs(start.x, start.y);
-//        }
-//        System.out.println("종료");
+        while(true) {
+            init();
+            bfs(start.x, start.y);
+
+            if (minX != Integer.MAX_VALUE && minY != Integer.MAX_VALUE) {
+                count += fishDistance[minX][minY];
+                eatFish++;
+
+                if (eatFish == shark) {
+                    shark++;
+                    eatFish = 0;
+                }
+
+                map[minX][minY] = 0;
+
+                start.x = minX;
+                start.y = minY;
+            } else {
+                break;
+            }
+        }
+        System.out.println(count);
     }
 
-    public static void move() {
-        for(Point p : fishList) {
-            bfs(p.x, p.y);
+    /* 최소 거리와 그를 충족하는 좌표를 초기화한다. */
+    public static void init() {
+        minDist = Integer.MAX_VALUE;
+        minX = Integer.MAX_VALUE;
+        minY = Integer.MAX_VALUE;
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++) {
+                fishDistance[i][j] = -1;
+            }
         }
     }
 
-    public static void fishSort() {
-        Collections.sort(fishList);
-    }
-
-    //이때의 좌표는 도착 x, y
+    /*
+    * param : shark의 시작점
+    * */
     public static void bfs(int x, int y) {
         Queue<Point> q = new LinkedList<>();
+        fishDistance[x][y] = 0;
         q.add(new Point(x, y));
 
         while(!q.isEmpty()) {
             Point p = q.poll();
+            x = p.x;
+            y = p.y;
 
             for(int i=0; i<4; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] > shark) {
+                /* 범위 체크 & 방문 체크 & shark보다 큰지 */
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) {
+                    continue;
+                }
+                if(map[nx][ny] > shark || fishDistance[nx][ny] != -1 ){
                     continue;
                 }
 
-                if(map[nx][ny] == 0) {
-                    count++;
-                    q.add(new Point(nx, ny));
-                } else{
-                    System.out.println("물고기 먹음");
-                    eatFish++;
-                    totalFish--;
-                    map[nx][ny] = 0;
+                fishDistance[nx][ny] = fishDistance[x][y] + 1;
+
+                //먹을 수 있는 물고기
+                if( map[nx][ny] >= 1 && map[nx][ny] <= 6 && map[nx][ny] < shark ) {
+                    if(minDist > fishDistance[nx][ny]){
+                        minX = nx;
+                        minY = ny;
+                        minDist = fishDistance[nx][ny];
+                    }
+                    else if(minDist == fishDistance[nx][ny]){
+                        if(minX == nx){
+                            if(minY > ny){
+                                minX = nx;
+                                minY = ny;
+                            }
+                        } else if(minX > nx){
+                            minX = nx;
+                            minY = ny;
+                        }
+                    }
                 }
+                q.add(new Point(nx, ny));
             }
         }
     }
 
-    static class Point implements Comparable<Point> {
+    static class Point {
         int x;
         int y;
 
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        @Override
-        public int compareTo(Point p) {
-            if(this.x < p.getX()) {
-                return -1;
-            } else if ( this.x == p.getX() ) {
-                return 0;
-            } else {
-                return 1;
-            }
         }
     }
 }
