ebfee28d09044780e385d44f8dac5b78a83f7179
inaeLee <inae9709@hanmail.net>
2020-04-25 00:28:09 +0900

    Fix Swap function

--- src/Sort/BubbleSort.java
+++ src/Sort/BubbleSort.java
@@ -22,10 +22,10 @@ public class BubbleSort {
     }
 
     static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = arr.length-1; j > i; j--) {
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = arr.length - 1; j > i; j--) {
                 if (arr[j] < arr[j - 1]) {
-                    swap(arr, j, j-1);
+                    swap(arr, j, j - 1);
                 }
             }
         }
@@ -33,7 +33,7 @@ public class BubbleSort {
 
     static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
-        arr[a] = b;
+        arr[a] = arr[b];
         arr[b] = temp;
     }
 }
