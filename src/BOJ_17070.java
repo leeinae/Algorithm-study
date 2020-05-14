a9ae9a889656b77c831ef4aa47680d371260fc52
inaeLee <inae9709@hanmail.net>
2020-05-14 21:25:56 +0900

    [BOJ] 파이프 옮기기_DFS+DP로 풀기

--- src/BOJ_17070.java
+++ src/BOJ_17070.java
@@ -5,9 +5,10 @@ import java.util.StringTokenizer;
 public class BOJ_17070 {
     static int n;
     static int[][] map;
+    static int[][][] dp;
     static int[] dx = {0, 1, 1}; //가, 세, 대
     static int[] dy = {1, 0, 1};
-    static int count = 0;
+    static int count;
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,59 +16,54 @@ public class BOJ_17070 {
         n = Integer.parseInt(br.readLine());
 
         map = new int[n+1][n+1];
+        dp = new int[n+1][n+1][3];
 
         for (int i = 1; i <= n; i++) {
              st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= n; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                for (int k = 0; k < 3; k++) {
+                    dp[i][j][k] = -1;
+                }
             }
         }
 
-        solution(new Pipe(1,2, 0));
-
+        count = dfs(1, 2, 0);
         System.out.println(count);
-    }
-
-    static void solution(Pipe pipe) {
-        if (pipe.x == n && pipe.y == n) {
-            count++;
-            return;
-        }
-
-        for (int i = 0; i < 3; i++) {
-            if ((pipe.dir == 0 && i == 1) || (pipe.dir == 1 && i == 0)) {
-                //가로일때는 가로, 대각선으로만 이동 가능
-                continue;
-            }
-
-            int nx = pipe.x + dx[i];
-            int ny = pipe.y + dy[i];
-
-            if (nx > n || ny > n || map[nx][ny] == 1) {
-                continue;
-            }
-
-            if (i == 2 && (map[pipe.x][pipe.y + 1] == 1 || map[pipe.x + 1][pipe.y] == 1)) {
-                continue;
-            }
-
-            solution(new Pipe(nx, ny, i));
-        }
-
     }
 
     // 가로 0
     // 세로 1
     // 대각선 2
-    static class Pipe {
-        int x;
-        int y;
-        int dir;
-
-        public Pipe(int x, int y, int dir) {
-            this.x = x;
-            this.y = y;
-            this.dir = dir;
+    static int dfs(int x, int y, int dir) {
+        if (x > n || y > n || map[x][y] == 1) {
+            return 0;
         }
+
+        if (dir == 2 && (map[x][y - 1] == 1 || map[x - 1][y] == 1)) {
+            return 0;
+        }
+
+        if (dp[x][y][dir] != -1) {
+            return dp[x][y][dir];
+        }
+
+        if (x == n && y == n) {
+            return 1;
+        }
+
+        int sum =0;
+        for (int i = 0; i < 3; i++) {
+            if ((dir == 0 && i == 1) || (dir == 1 && i == 0)) {
+                continue;
+            }
+
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            sum += dfs(nx, ny, i);
+        }
+
+        return dp[x][y][dir] = sum;
     }
 }
