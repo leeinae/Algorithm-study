506a4d78d00fbb5dc48372b0b58000e7abd10c9d
inaeLee <inae9709@hanmail.net>
2020-05-26 23:23:34 +0900

    [BOJ]2163_초콜릿 자르기_20/05/26

---
+++ src/BOJ_2163.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_2163 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        System.out.println(m * n - 1);
+    }
+}
