c71f8fbad73eac8bd4f6b024cd1ec151fbd870ad
inaeLee <inae9709@hanmail.net>
2020-06-08 17:46:42 +0900

    [BOJ]1965_상자넣기_20/06/08

---
+++ src/BOJ_1965.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BOJ_1965 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int dp[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                    }
+                }
+            }
+            max = Math.max(dp[i], max);
+        }
+
+        System.out.println(max);
+    }
+}
