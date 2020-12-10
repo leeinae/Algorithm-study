c05e69a45b26fda18abe4cfc7f8e86d125be189d
inaeLee <inae9709@hanmail.net>
2020-12-10 20:41:46 +0900

    [BOJ] 11055_가장 큰 증가 부분 수열_re_20/12/10

---
+++ src/BOJ_11055_re.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11055_re {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = arr[i];
+        }
+
+        int max = dp[0];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + arr[i]) {
+                    dp[i] = dp[j] + arr[i];
+                }
+
+                max = Math.max(dp[i], max);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
